ad49aec1793e0610a45b504d3168af9edcf0de4a
bigbusinessborsalino <mrsupreetshosalli@gmail.com>
2025-03-01 10:13:09 +0530

    Create init.py

---
+++ bot/init.py
@@ -0,0 +1,4 @@
+from .handlers import search, episodes, quality, download
+from .services import ZoroScraper, GogoAnimeScraper, NyaaScraper
+
+__all__ = ['search', 'episodes', 'quality', 'download', 'ZoroScraper', 'GogoAnimeScraper', 'NyaaScraper']
